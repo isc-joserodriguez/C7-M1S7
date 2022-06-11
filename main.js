0759ad8318f33e0764762d8d95f197a86b26137b
Jose Rodriguez <isc.joserodriguez@gmail.com>
2022-06-11 10:12:13 -0600

    Ejemplos

--- main.js
+++ main.js
@@ -34,3 +34,48 @@ console.log(`cadenas ${numero1}`); //Backtic
 
 console.log(`Si sumo ${numero1} + ${numero2} mi valor es:
 ${(numero1 + numero2)}`);
+
+
+
+let arreglo = [1, 2, 3, 4, 5, 6];
+
+console.log(arreglo[4]);
+
+let auto = {
+    año: 1990,
+    color: 'Rojo',
+    dueños: ['Mario', 'Mauricio', 'Paty', 'María'],
+    marca: 'Ford',
+    pago: false,
+    accesorios: {
+        bocinas: 'Marca cara',
+        llantas: 'Micheline',
+        canciones: ['Pollito', 'Vaca lola']
+    },
+    "que tiene espacios": 'hola',
+    propiedadEspacios: 'HEYYYY'
+}
+
+console.log(auto.marca);
+let propiedad = 'accesorios'
+let propiedad2 = 'canciones'
+console.log(auto[propiedad][propiedad2]);
+console.log(auto.accesorios.canciones);
+let propiedadEspacios = "que tiene espacios";
+console.log(auto[propiedadEspacios]);
+console.log(auto.propiedadEspacios);
+
+
+auto["que tiene espacios"] = 110;
+auto.marca = "Nissan"
+
+auto.nuevaPropiedad = 'Un Valor'
+
+delete auto.propiedadEspacios;
+
+console.log(auto);
+
+
+function mostrarObjeto(objetoParam) {
+    console.log(objetoParam);
+}
