b2af1e5c014b6fb1342d10d9be792189bb13b4af
Jose Rodriguez <isc.joserodriguez@gmail.com>
2022-06-11 09:38:34 -0600

    string templates

--- main.js
+++ main.js
@@ -5,10 +5,10 @@
  */
 var numero = 10;
 
-if(true){
+if (true) {
     var numero = 20;
     console.log(numero)
-}else{
+} else {
     console.log(numero)
 }
 console.log(numero);
@@ -16,11 +16,21 @@ console.log(numero);
 
 let cadena = 'Cadena1';
 
-if(false){
+if (false) {
     let cadena = 'Cadena2';
     console.log(cadena)
-}else{
+} else {
     let cadena = 'Cadena3';
     console.log(cadena)
 }
 console.log(cadena);
+
+//string templates
+let numero1 = 5;
+let numero2 = 10;
+console.log("Si sumo " + numero1 + " + " + numero2 + " mi valor es:\n" + (numero1 + numero2));
+
+console.log(`cadenas ${numero1}`); //Backtic
+
+console.log(`Si sumo ${numero1} + ${numero2} mi valor es:
+${(numero1 + numero2)}`);
