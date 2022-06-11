91dcc5908e810e058e26a2df8f76b694482c5556
Jose Rodriguez <isc.joserodriguez@gmail.com>
2022-06-11 11:16:33 -0600

    Objetos- ejemplo

---
+++ agenda.js
@@ -0,0 +1,62 @@
+let objetoEjercicio={};
+let contactos = [
+    {
+        id: 1240,
+        nombre: 'Mario',
+        apellido: 'Ramos',
+        telefono: '3111234567'
+    },
+    {
+        id: 10,
+        nombre: 'Mario',
+        apellido: 'Ramos',
+        telefono: '3121234567'
+    },
+    {
+        id: 1000,
+        nombre: 'Mario',
+        apellido: 'Perez',
+        telefono: '3121254567'
+    },
+];
+
+/*
+agregarContacto(nombre, apellido, telefono);
+eliminarContacto(id);
+*/
+
+function agregarContacto(id, nombre, apellido, telefono) {
+    // contactos.push(elemento) agrega elemento al final
+    // contactos.unshift(elemento) agrega elemento al inicio
+    // contactps.pop() elimina el último elemento
+    // contactps.shift() elimina el primer elemento
+    contactos.push({
+        id,
+        nombre,
+        apellido: apellido,
+        telefono: telefono,
+    })
+}
+
+/* function eliminarContacto(id) {
+    contactos.forEach(function (contacto, indice) {
+        if (contacto.id === id) {
+            contactos.splice(indice, 1)
+        }
+    });
+}  */
+
+function eliminarContacto(nombre, apellido, telefono) {
+    let contador = 0;
+    contactos.forEach(function (contacto, indice) {
+        if (
+            contador === 0
+            && contacto.nombre === nombre
+            && contacto.apellido === apellido
+            && contacto.telefono === telefono
+        ) {
+            contactos.splice(indice, 1);
+            contador++;
+        }
+    });
+}
